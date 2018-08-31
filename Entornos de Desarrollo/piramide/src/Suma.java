46286ffb9dea10a51a5ab7184dd6623adbb05175
Mario <marioarnedo1@gmail.com>
2018-08-31 10:17:03 +0200

    Ejercicio Java

---
+++ Entornos de Desarrollo/piramide/src/Suma.java
@@ -0,0 +1,16 @@
+
+public class Suma {
+	
+	private int sumando1, sumando2;
+	
+	public Suma(int n1, int n2){
+		sumando1 = n1;
+		sumando2 = n2;
+	}
+	
+	public int sumar(){
+		int resultado = sumando1 + sumando2;
+		return resultado;
+	}
+
+}
---
+++ Entornos de Desarrollo/piramide/src/SumaTest.java
@@ -0,0 +1,22 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class SumaTest {
+
+	@Test
+	public void sumaPositivios() {
+		
+	}
+
+	@Test
+	public void sumaNegativos() {
+		
+	}
+	
+	@Test
+	public void sumaPositivioNegativo() {
+		
+	}
+}
